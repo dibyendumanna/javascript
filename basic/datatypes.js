255b22cc88e9e676a4918e13a65d1c0a9dd90d98
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-04 11:19:48 +0000

    datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,14 @@
+"use strict " //treat as new js
+
+//alert(2+3)// it will not work in nodeJs
+
+console.log(typeof null);
+
+console.log(typeof undefined);
+
+//null
+// undefinrd
+// string
+// number
+// bigint
+// object
--- basic/test.js
+++ basic/test.js
@@ -1,3 +1,4 @@
 console.log("dibyendu");
 let k = 19;
 console.log(k);
+MediaKeySession
