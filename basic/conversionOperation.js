ad252b052ac8755ce31cfff165b652460910f0ff
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-06 15:20:02 +0000

    conversionOperation

--- basic/conversionOperation.js
+++ basic/conversionOperation.js
@@ -1,4 +1,4 @@
-let score = "33ab";
+/*let score = "33ab";
 
 console.log(typeof score);
 
@@ -26,6 +26,43 @@ let a = 23
 let b = a + string
 console.log(b);
 console.log(typeof b);
+
+*/
+//*******************OPERATIONS */
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+let str1 = "Hello "
+let str2 = "Dibyendu"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log( 1 + "2" + 2);
+console.log(1 + 3 + "2");
+
+
+console.log(true);
+console.log(+true);
+console.log(+"");
+
+
+let num1 , num2 , num3;
+num1 = num2= num3 = 2+2;
+
+let counter = 100
+counter++
+console.log(counter);
+
+console.log("2" > 1);
+
+
+
+
+
+
 
 
 
