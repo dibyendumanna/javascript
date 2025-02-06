7a4894e0e2e0ea3002b5e8b1f777084bc9547824
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-06 14:00:15 +0000

    conversion of data types

---
+++ basic/conversionOperation.js
@@ -0,0 +1,31 @@
+let score = "33ab";
+
+console.log(typeof score);
+
+let valueInInteger= Number(score);
+
+console.log(typeof valueInInteger);
+console.log(valueInInteger);
+
+// "33" => 33
+// "33ab" => NaN- not a number
+// true = > 1
+// false => 0
+
+// 1 => true
+// 0 => false
+// "" => false
+// "dibyendu" => true
+
+let number = 33;
+
+let string = String(number);
+console.log(string);
+console.log(typeof string);
+let a = 23
+let b = a + string
+console.log(b);
+console.log(typeof b);
+
+
+
