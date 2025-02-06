3f477377401a7640dfe77edafeb346fa103bea14
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-06 19:23:39 +0000

    date array

---
+++ basic/array.js
@@ -0,0 +1,3 @@
+const arry = [2, 4, 6, 8, 9]
+const heroes = ["captain ", "shaktiman", "krishna"]
+console.log(heroes[1]);
---
+++ basic/date.js
@@ -0,0 +1,10 @@
+let date = new Date()
+console.log(date);
+console.log(date.toDateString());
+console.log(date.toISOString());
+console.log(date.toLocaleDateString());
+console.log(date.toLocaleTimeString());
+console.log(typeof date);
+
+
+
