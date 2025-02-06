fed18bb78dad41f51563e20e7e80bd9a98472107
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-06 16:46:53 +0000

    math and num

---
+++ basic/Strings.js
@@ -0,0 +1,18 @@
+let naame = "Dibyendu"
+let count = 40
+console.log(naame + count + " value");
+console.log(`hello my name is ${naame} and my count is ${count}`);
+
+
+const gameName = new String("dibyendu");
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+
+
+
+const newString1 = "   dibyendu      2"
+console.log(newString1);
+console.log(newString1.trim());
+
+const url =  "diby.com/dibyendu%20manna"
+console.log(url.replace('%20','-'))
---
+++ basic/num_maths.js
@@ -0,0 +1,19 @@
+// const score = 39
+// const balance = new Number(100);
+// console.log(balance);
+// console.log(balance.toFixed(2));
+// console.log(balance.toPrecision(2));
+
+// const hnudreds = 10000000
+// console.log(hnudreds.toLocaleString('en-IN'));
+
+// ------------MATHS-------------
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(5.7));
+// console.log(Math.ceil(7.1));
+// console.log(Math.min(7.1, 9,23,34));
+
+console.log(Math.random());
+//console.log(Math.random()*3+1);
