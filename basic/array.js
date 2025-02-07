18799fa4c3c8659694272d696797e75f17a35e78
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-07 09:05:11 +0000

    array

--- basic/array.js
+++ basic/array.js
@@ -1,3 +1,23 @@
 const arry = [2, 4, 6, 8, 9]
 const heroes = ["captain ", "shaktiman", "krishna"]
 console.log(heroes[1]);
+
+// arry.push(7)
+// console.log(arry);
+// arry.pop()
+// console.log(arry);
+
+// newArr = arry.join()
+// console.log(newArr);
+
+//slice , splice
+
+myarr1= arry.slice(0, 3)
+console.log(myarr1);
+
+myarr2 = arry.splice(0, 2)
+console.log(arry);
+console.log(myarr2);
+
+
+
