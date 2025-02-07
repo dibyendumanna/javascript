5c9b5c9efa2ab38f21857f29988155ea2277aabf
dibyendumanna <80017534+dibyendumanna@users.noreply.github.com>
2025-02-07 09:30:16 +0000

    array2

---
+++ basic/array2.js
@@ -0,0 +1,25 @@
+const marvelHero = ["Thor" , "ironMan" , "captainAmerica" , "spiderMan"]
+const dcHero = ["Superman" , "flash" , "batMan"]
+
+// marvelHero.push(dcHero)
+
+// console.log(marvelHero);
+// console.log(marvelHero[4][1]);
+
+// const allHeros = marvelHero.concat(dcHero)
+// console.log(allHeros);
+
+// const newHeros = [...marvelHero, ...dcHero]
+// console.log(newHeros);
+
+// const anotherArray =[1, 2, 3,[4, 5, 6], 7, [6, 7,[4,5]]]
+// const realArray = anotherArray.flat(1)
+// console.log(anotherArray);
+// console.log(realArray);
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1, score2, score3));
